fb5718a71dba92af46ab81704b8bc0a2ec175e70
willems57 <jeanwillems@hotmail.fr>
2025-01-03 19:59:49 +0100

    ajout de l'entite trajet

---
+++ StartUp/src/Entity/Ride.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RideRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RideRepository::class)]
+class Ride
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $departure = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $arrival = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $availableSeats = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDateTime = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDateTime = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $ecoFriendly = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDeparture(): ?string
+    {
+        return $this->departure;
+    }
+
+    public function setDeparture(string $departure): static
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    public function getArrival(): ?string
+    {
+        return $this->arrival;
+    }
+
+    public function setArrival(string $arrival): static
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAvailableSeats(): ?int
+    {
+        return $this->availableSeats;
+    }
+
+    public function setAvailableSeats(int $availableSeats): static
+    {
+        $this->availableSeats = $availableSeats;
+
+        return $this;
+    }
+
+    public function getStartDateTime(): ?\DateTimeInterface
+    {
+        return $this->startDateTime;
+    }
+
+    public function setStartDateTime(\DateTimeInterface $startDateTime): static
+    {
+        $this->startDateTime = $startDateTime;
+
+        return $this;
+    }
+
+    public function getEndDateTime(): ?\DateTimeInterface
+    {
+        return $this->endDateTime;
+    }
+
+    public function setEndDateTime(\DateTimeInterface $endDateTime): static
+    {
+        $this->endDateTime = $endDateTime;
+
+        return $this;
+    }
+
+    public function isEcoFriendly(): ?bool
+    {
+        return $this->ecoFriendly;
+    }
+
+    public function setEcoFriendly(?bool $ecoFriendly): static
+    {
+        $this->ecoFriendly = $ecoFriendly;
+
+        return $this;
+    }
+}
