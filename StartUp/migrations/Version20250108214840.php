551016450f16fb1a16edf10e58c201c5ffda4662
willems57 <jeanwillems@hotmail.fr>
2025-01-08 22:49:08 +0100

    creation de l'entiter voiture et de sa migration

---
+++ StartUp/migrations/Version20250108214840.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250108214840 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE voiture (id INT AUTO_INCREMENT NOT NULL, voiture VARCHAR(255) NOT NULL, dateimat DATE NOT NULL, fumeur TINYINT(1) NOT NULL, annimaux TINYINT(1) NOT NULL, marque VARCHAR(255) NOT NULL, place INT NOT NULL, modele VARCHAR(255) NOT NULL, couleur VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE voiture');
+    }
+}
---
+++ StartUp/src/Entity/Voiture.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $voiture = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateimat = null;
+
+    #[ORM\Column]
+    private ?bool $fumeur = null;
+
+    #[ORM\Column]
+    private ?bool $annimaux = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $marque = null;
+
+    #[ORM\Column]
+    private ?int $place = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modele = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $couleur = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVoiture(): ?string
+    {
+        return $this->voiture;
+    }
+
+    public function setVoiture(string $voiture): static
+    {
+        $this->voiture = $voiture;
+
+        return $this;
+    }
+
+    public function getDateimat(): ?\DateTimeInterface
+    {
+        return $this->dateimat;
+    }
+
+    public function setDateimat(\DateTimeInterface $dateimat): static
+    {
+        $this->dateimat = $dateimat;
+
+        return $this;
+    }
+
+    public function isFumeur(): ?bool
+    {
+        return $this->fumeur;
+    }
+
+    public function setFumeur(bool $fumeur): static
+    {
+        $this->fumeur = $fumeur;
+
+        return $this;
+    }
+
+    public function isAnnimaux(): ?bool
+    {
+        return $this->annimaux;
+    }
+
+    public function setAnnimaux(bool $annimaux): static
+    {
+        $this->annimaux = $annimaux;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): static
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getPlace(): ?int
+    {
+        return $this->place;
+    }
+
+    public function setPlace(int $place): static
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
