ab414921388758f790bd43a92eaf3d12bd39fd91
willems57 <jeanwillems@hotmail.fr>
2025-01-09 00:00:44 +0100

    creation de trajets en cous et de sa migration

---
+++ StartUp/src/Entity/Trajetsencours.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrajetsencoursRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TrajetsencoursRepository::class)]
+class Trajetsencours
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $depart = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $arrive = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $duree = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDepart(): ?string
+    {
+        return $this->depart;
+    }
+
+    public function setDepart(string $depart): static
+    {
+        $this->depart = $depart;
+
+        return $this;
+    }
+
+    public function getArrive(): ?string
+    {
+        return $this->arrive;
+    }
+
+    public function setArrive(string $arrive): static
+    {
+        $this->arrive = $arrive;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+}
