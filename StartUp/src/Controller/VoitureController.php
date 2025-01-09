dc0e5c397f2f6a62b46424cd91328586611bd956
willems57 <jeanwillems@hotmail.fr>
2025-01-09 19:36:46 +0100

    initialisation de voiture controller

---
+++ StartUp/src/Controller/VoitureController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class VoitureController extends AbstractController
+{
+    #[Route('/voiture', name: 'app_voiture')]
+    public function index(): Response
+    {
+        return $this->render('voiture/index.html.twig', [
+            'controller_name' => 'VoitureController',
+        ]);
+    }
+}
