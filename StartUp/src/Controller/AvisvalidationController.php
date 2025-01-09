e6dc8d471feab7bbc2a207daada2ed4778d2941c
willems57 <jeanwillems@hotmail.fr>
2025-01-09 19:29:04 +0100

    configuration de avisvalidation controlller

--- StartUp/src/Controller/AvisvalidationController.php
+++ StartUp/src/Controller/AvisvalidationController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 
-use App\Entity\Avis;
-use App\Repository\AvisRepository;
+use App\Entity\Avisvalidation;
+use App\Repository\AvisvalidationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,53 +15,53 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AvisvalidationController extends AbstractController
 {
 
-    #[Route('/api/avis', methods: ['POST'])]
-    public function addAvis(Request $request, EntityManagerInterface $em): JsonResponse
+    #[Route('/api/avisvalidation', methods: ['POST'])]
+    public function addAvisvalidation(Request $request, EntityManagerInterface $em): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
-        $avis = new Avis();
-        $avis->setName($data['name']);
-        $avis->setCommentaire($data['commentaire']);
-        $avis->setDate(new \DateTime($data['date']));
-        $avis->setConducteur($data['conducteur']);
+        $avisvalidation = new Avisvalidation();
+        $avisvalidation->setName($data['name']);
+        $avisvalidation->setCommentaire($data['commentaire']);
+        $avisvalidation->setDate(new \DateTime($data['date']));
+        $avisvalidation->setConducteur($data['conducteur']);
 
-        $em->persist($avis);
+        $em->persist($avisvalidation);
         $em->flush();
 
-        return $this->json(['status' => 'Avis created'], 201);
+        return $this->json(['status' => 'Avisvalidation created'], 201);
     }
 
 
-    #[Route('/api/avis', methods: ['GET'])]
-    public function getAllAvis(AvisRepository $avisRepository): JsonResponse
+    #[Route('/api/avisvalidation', methods: ['GET'])]
+    public function getAllAvisvalidation(AvisvalidationRepository $avisvalidationRepository): JsonResponse
     {
-        $avis = $avisRepository->findAll();
+        $avisvalidation = $avisvalidationRepository->findAll();
 
-        $data = array_map(fn(Avis $avis) => [
-            'id' => $avis->getId(),
-            'name' => $avis->getName(),
-            'commentaire' => $avis->getCommentaire(),
-            'date' => $avis->getDate()->format('Y-m-d'),
-            'conducteur' => $avis->getConducteur(),
-        ], $avis);
+        $data = array_map(fn(Avisvalidation $avisvalidation) => [
+            'id' => $avisvalidation->getId(),
+            'name' => $avisvalidation->getName(),
+            'commentaire' => $avisvalidation->getCommentaire(),
+            'date' => $avisvalidation->getDate()->format('Y-m-d'),
+            'conducteur' => $avisvalidation->getConducteur(),
+        ], $avisvalidation);
 
         return $this->json($data);
     }
 
 
-    #[Route('/api/avis/{id}', methods: ['DELETE'])]
-    public function deleteAvis(int $id, AvisRepository $avisRepository, EntityManagerInterface $em): JsonResponse
+    #[Route('/api/avisvalidation/{id}', methods: ['DELETE'])]
+    public function deleteAvisvalidation(int $id, AvisvalidationRepository $avisvalidationRepository, EntityManagerInterface $em): JsonResponse
     {
-        $avis = $avisRepository->find($id);
+        $avisvalidation = $avisvalidationRepository->find($id);
 
-        if (!$avis) {
-            return $this->json(['error' => 'Avis not found'], 404);
+        if (!$avisvalidation) {
+            return $this->json(['error' => 'Avisvalidation not found'], 404);
         }
 
-        $em->remove($avis);
+        $em->remove($avisvalidation);
         $em->flush();
 
-        return $this->json(['status' => 'Avis deleted'], 200);
+        return $this->json(['status' => 'Avisvalidation deleted'], 200);
     }
 }
