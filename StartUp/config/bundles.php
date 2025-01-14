098cdf401efc41932dec63a908d81665146d852b
willems57 <jeanwillems@hotmail.fr>
2025-01-14 15:30:46 +0100

    instalation des composant pour nelmio

--- StartUp/config/bundles.php
+++ StartUp/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
