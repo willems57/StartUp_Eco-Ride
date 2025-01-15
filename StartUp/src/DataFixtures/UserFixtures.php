858ac5a706a8f16abe68b4ce710933c539c00aba
willems57 <jeanwillems@hotmail.fr>
2025-01-15 16:44:27 +0100

    fixture user configurer ,qis pqs executer

--- StartUp/src/DataFixtures/UserFixtures.php
+++ StartUp/src/DataFixtures/UserFixtures.php
@@ -4,20 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         for ($i = 1; $i <= 20; $i++) {
         $user = (new User())
             ->setFirstName('firstName' . $i)
             ->setLastName('lastName' . $i)
-            ->setCredit('credit' . $i)
-            ->setEmail('email' . $i)
-            ->setRole('role' . $i)
+            ->setCredits('credits' . $i)
+            ->setRoles(['ROLE_USER'])
+            ->setEmail('email' . $i);
             $user->setPassword($this->passwordHasher->hashPassword($user, 'password' . $i));
-
         $manager->persist($user);
     }
 
