c855c90f6db6bca9e6f635fed3af379aabe6a71e
willems57 <jeanwillems@hotmail.fr>
2025-01-16 20:15:01 +0100

    pris en compte des autantification pour l'accessibiliter au route telle que defini dans route et router.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,19 +2,19 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/acceuil.html"),
-    new Route("/signin", "signin", "/auth/signin.html"),
-    new Route("/signup", "signup", "/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/contact", "contact", "/pages/contact.html"),
-    new Route("/administrateur", "administrateur", "/admin/administrateur.html"),
-    new Route("/trajets", "trajets", "/pages/covoiturage/trajets.html"),
-    new Route("/avis", "Avis", "/pages/avis.html", "/js/avis.js"),
-    new Route("/signup2", "signupA", "/admin/signup2.html", "/admin/signup2.js"),
-    new Route("/suspendu", "suspendu", "/admin/suspendu.html", "/admin/suspendu.js"),
-    new Route("/utilisateurs", "utilisateurs", "/pages/utilisateurs/utilisateurs.html"),
-    new Route("/abonees", "abonees", "/admin/abonees.html"),
-    new Route("/employers", "employers", "/pages/employers/employers.html"),
-    new Route("/abonees", "abonees", "/admin/abonees.html"),
+    new Route("/", "Accueil", "/pages/acceuil.html", []),
+    new Route("/signin", "signin", "/auth/signin.html", []),
+    new Route("/signup", "signup", "/auth/signup.html", [], "/js/auth/signup.js"),
+    new Route("/contact", "contact", "/pages/contact.html", [], "/js/contact.js"),
+    new Route("/administrateur", "administrateur", "/admin/administrateur.html", []),
+    new Route("/trajets", "trajets", "/pages/covoiturage/trajets.html", []),
+    new Route("/avis", "Avis", "/pages/avis.html", [], "/js/avis.js"),
+    new Route("/signup2", "signupA", "/admin/signup2.html", [], "/admin/signup2.js"),
+    new Route("/suspendu", "suspendu", "/admin/suspendu.html", [], "/admin/suspendu.js"),
+    new Route("/utilisateurs", "utilisateurs", "/pages/utilisateurs/utilisateurs.html", []),
+    new Route("/abonees", "abonees", "/admin/abonees.html", []),
+    new Route("/employers", "employers", "/pages/employers/employers.html", []),
+    new Route("/abonees", "abonees", "/admin/abonees.html", []),
 
 ];
 
