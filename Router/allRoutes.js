4a6429b74e901770b5e8cb2ed4c44c231a78d682
willems57 <jeanwillems@hotmail.fr>
2025-01-16 17:35:19 +0100

    ajout et configuration de la page avis avec des modale comme pour ma page acceuil + comfiguration de l'acces a la page avis dans allRoute.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/acceuil.html"),
+    new Route("/avis", "Avis", "/pages/avis.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
