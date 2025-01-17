1bc8fd59e671739a6be5e840c45c74b5d5d52cf8
willems57 <jeanwillems@hotmail.fr>
2025-01-17 21:54:43 +0100

    mise en place du superadmin et lancemen de sa fixture

--- StartUp/src/Controller/AdminController.php
+++ StartUp/src/Controller/AdminController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/users')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    #[Route('/', methods: ['GET'])]
+    public function listUsers(UserRepository $userRepository): JsonResponse
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+        $users = $userRepository->findAll();
+
+        $data = array_map(fn($user) => [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ], $users);
+
+        return $this->json($data);
+    }
+
+    #[Route('/{id}/role', methods: ['PUT'])]
+    public function changeUserRole(
+        User $user,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['roles'])) {
+            $user->setRoles($data['roles']);
+            $entityManager->flush();
+
+            return $this->json(['message' => 'Rôles mis à jour avec succès.']);
+        }
+
+        return $this->json(['error' => 'Aucun rôle fourni.'], 400);
     }
 }
