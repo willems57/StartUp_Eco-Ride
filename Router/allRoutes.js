0689b0ac8ea0d6137a347bed8384907e1d87a41e
willems57 <jeanwillems@hotmail.fr>
2025-01-18 19:52:21 +0100

    modification de index.html et de allRoute.js pour limitees les acces en fonction des role

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,14 +6,14 @@ export const allRoutes = [
     new Route("/signin", "signin", "/auth/signin.html", [], "/js/auth/signin.js"),
     new Route("/signup", "signup", "/auth/signup.html", [], "/js/auth/signup.js"),
     new Route("/contact", "contact", "/pages/contact.html", [], "/js/contact.js"),
-    new Route("/administrateur", "administrateur", "/admin/administrateur.html", [], "/admin/administrateur.js"),
+    new Route("/administrateur", "administrateur", "/admin/administrateur.html", ["ROLE_ADMIN"], "/admin/administrateur.js"),
     new Route("/trajets", "trajets", "/pages/covoiturage/trajets.html", [], "/pages/covoiturage/trajets.js"),
-    new Route("/avis", "Avis", "/pages/avis.html", [], "/js/avis.js"),
-    new Route("/signup2", "signupA", "/admin/signup2.html", [], "/admin/signup2.js"),
-    new Route("/suspendu", "suspendu", "/admin/suspendu.html", [], "/admin/suspendu.js"),
-    new Route("/utilisateurs", "utilisateurs", "/pages/utilisateurs/utilisateurs.html", [], "/pages/utilisateurs/utilisateurs.js"),
-    new Route("/abonees", "abonees", "/admin/abonees.html", [], "/admin/abonees.js"),
-    new Route("/employers", "employers", "/pages/employers/employers.html", [], "/pages/employers/employers.js"),
+    new Route("/avis", "Avis", "/pages/avis.html", ["ROLE_USER"], "/js/avis.js"),
+    new Route("/signup2", "signupA", "/admin/signup2.html", ["ROLE_ADMIN"], "/admin/signup2.js"),
+    new Route("/suspendu", "suspendu", "/admin/suspendu.html", ["ROLE_ADMIN"], "/admin/suspendu.js"),
+    new Route("/utilisateurs", "utilisateurs", "/pages/utilisateurs/utilisateurs.html", ["ROLE_USER"], "/pages/utilisateurs/utilisateurs.js"),
+    new Route("/abonees", "abonees", "/admin/abonees.html", ["ROLE_ADMIN"], "/admin/abonees.js"),
+    new Route("/employers", "employers", "/pages/employers/employers.html", ["ROLE_EMPLOYER"], "/pages/employers/employers.js"),
 
 ];
 
